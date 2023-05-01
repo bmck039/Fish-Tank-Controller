05bf67930293ebd0d4b06b61b72366e8aab92024
bmck039 <mckallipb@southwestern.edu>
2023-05-01 14:01:03 -0500

    Updated scheduling so that it only checks for updates once per day

--- docker.py
+++ docker.py
@@ -11,6 +11,9 @@ def checkUpdate():
 if __name__ == "__main__":
     webServer.startServer()
     time.sleep(60 * (60 - datetime.datetime.now().minute))
+    previous_day = None
     while True:
-        checkUpdate()
+        if previous_day != datetime.datetime.now().day: #check for update every day
+            previous_day = datetime.datetime.now().day
+            checkUpdate()
         time.sleep(60 * 60)
