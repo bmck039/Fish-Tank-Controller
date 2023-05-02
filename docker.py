9c9b04b515a64608d011616b6c58d458ba1d8cde
bmck039 <mckallipb@southwestern.edu>
2023-05-02 00:54:37 -0500

    Fixed import bug with webServer

--- docker.py
+++ docker.py
@@ -2,10 +2,12 @@ import datetime
 import time
 import subprocess
 import webServer
+import importlib
 
 def checkUpdate():
     webServer.stopServer()
     subprocess.run(["git", "pull"])
+    importlib.reload(webServer)
     webServer.startServer()
 
 if __name__ == "__main__":
@@ -13,7 +15,7 @@ if __name__ == "__main__":
     time.sleep(60 * (60 - datetime.datetime.now().minute))
     previous_day = None
     while True:
-        if previous_day != datetime.datetime.now().day: #check for update every day
+        if previous_day != datetime.datetime.now().day: #check for update once per day
             previous_day = datetime.datetime.now().day
             checkUpdate()
         time.sleep(60 * 60)
